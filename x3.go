0b408dfdb99e61f64b28ab5ce77ab95989bedb33
Adriabs <adrianskibelid@hotmail.com>
2017-01-26 12:47:41 +0100

    hei

---
+++ x3.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	. "fmt"
+	"net"
+)
+
+func main(){
+	localHost := "10.22.75.132"
+	ln, err := net.Listen("tcp", localHost+":34933")
+	if err != nil{
+		Println("Error!", err.Error())
+	}
+	Println("ln = ", ln)
+	conn, err := ln.Accept()
+	if err!= nil{
+		Println("Error!", err.Error())
+	}
+	Println("conn =", conn)
+}
